009127ab10c033a8ceffe2827c1b2344b8210cad
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-06-07 13:16:13 +0300

    some things were added

--- proj4 - Simple Web Browser/ViewController.swift
+++ proj4 - Simple Web Browser/ViewController.swift
@@ -6,12 +6,41 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, WKNavigationDelegate{
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
     override func viewDidLoad() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let url = URL(string: "https://youtube.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    @objc func openTapped(){
+        let ac = UIAlertController(title: "Open Page", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "youtube.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil  ))
+        
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true, completion: nil)
+                    
+    }
+    
+    func openPage(action: UIAlertAction){
+        guard let actionTitle = action.title else {return}
+        
+        guard let url = URL(string: "https://" + actionTitle) else {return}
+        webView.load(URLRequest(url: url))
     }
 
 
