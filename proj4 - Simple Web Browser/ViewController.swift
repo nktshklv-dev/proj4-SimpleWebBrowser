64c891077c0105beba9b1d1f50cae5402873cb36
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-06-08 11:42:40 +0300

    added the list of web pages and some things

--- proj4 - Simple Web Browser/ViewController.swift
+++ proj4 - Simple Web Browser/ViewController.swift
@@ -18,31 +18,35 @@ class ViewController: UIViewController, WKNavigationDelegate{
         view = webView
     }
     override func viewDidLoad() {
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         super.viewDidLoad()
         let url = URL(string: "https://youtube.com")!
         webView.load(URLRequest(url: url))
-        webView.allowsBackForwardNavigationGestures = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+       
     }
     
     @objc func openTapped(){
-        let ac = UIAlertController(title: "Open Page", message: nil, preferredStyle: .actionSheet)
+        let ac = UIAlertController(title: "Choose the website", message: nil, preferredStyle: .actionSheet)
         ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "youtube.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil  ))
-        
+        ac.addAction(UIAlertAction(title: "github.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(ac, animated: true, completion: nil)
-                    
     }
     
-    func openPage(action: UIAlertAction){
+    func openPage(_ action: UIAlertAction){
         guard let actionTitle = action.title else {return}
-        
         guard let url = URL(string: "https://" + actionTitle) else {return}
         webView.load(URLRequest(url: url))
     }
-
-
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title 
+    }
+    
+   
+    
+   
 }
 
